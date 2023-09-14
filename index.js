fb6e121823264ca43de865d3fa82d3bf1b0bf506
Uttam Nayak <uknayak001@gmail.com>
2023-09-14 12:13:03 +0530

    make a get api

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express()
 const port = 8000;
 
-app.listen(port,()=>{
+
+app.listen(port, () => {
     console.log(`running in the port ${port}`)
 })
 
-app.get('/',(req,res)=>{
-    res.send("hello world")
+app.get('/info', (req, res) => {
+    res.status(200).send({
+        name: "David",
+        phone: "123456789",
+        mail:"david@gmail.com",
+        desc :" making a websi"
+        });
 })
